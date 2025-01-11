d70086c88b4d78164a484096d865d506f36b4dcd
unknown <saadsyed950@gmail.com>
2025-01-11 09:54:53 +0530

    Variables declared

--- src/components/EmbedDashboard.tsx
+++ src/components/EmbedDashboard.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react";
+
 const EmbedDashboard = () => {
+  const tableauContainerRef = useRef<HTMLDivElement | null>(null);
+  const url = import.meta.env.TABLEAU_URL as string;
+  const api = import.meta.env.API_URI as string;
+
   return <div>EmbedDashboard</div>;
 };
 
