8c5ac8180fea84427a650c5bd70ead92c58896e8
unknown <saadsyed950@gmail.com>
2025-01-11 09:38:23 +0530

    TableauVizOptions interface declared

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="vite/client" />
 
-//Define interfaces of Window type for Tableau
+interface TableauVizOptions {
+  width?: string;
+  height?: string;
+  [key: string]: unknown;
+}
