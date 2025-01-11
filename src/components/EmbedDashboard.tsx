de3a1fdb9ff24fda2e625ec34f6a3b4da0f1712f
unknown <saadsyed950@gmail.com>
2025-01-11 09:59:30 +0530

    Loadscript function defined

--- src/components/EmbedDashboard.tsx
+++ src/components/EmbedDashboard.tsx
@@ -1,9 +1,26 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const EmbedDashboard = () => {
-  const tableauContainerRef = useRef<HTMLDivElement | null>(null);
+  const vizContainerRef = useRef<HTMLDivElement | null>(null);
   const url = import.meta.env.TABLEAU_URL as string;
   const api = import.meta.env.API_URI as string;
+
+  useEffect(() => {
+    const loadScript = () => {
+      const scriptElement = document.createElement("script");
+      scriptElement.src = api;
+      scriptElement.async = true;
+      document.body.appendChild(scriptElement);
+
+      scriptElement.onload = () => {
+        if (vizContainerRef.current) {
+          new window.tableau.Viz(vizContainerRef.current, url);
+        }
+      };
+    };
+
+    loadScript();
+  }, [api, url]);
 
   return <div>EmbedDashboard</div>;
 };
