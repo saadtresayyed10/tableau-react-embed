9fac4c8211d20080b31c67ac84d85280cc8969b6
unknown <saadsyed950@gmail.com>
2025-01-11 09:32:27 +0530

    In the beginning...

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-start items-center w-full">
+      <h1 className="text-4xl text-red-500 font-semibold">Mariyam</h1>
+    </div>
+  );
+};
+
+export default App;
