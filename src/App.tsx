41a3edc6ea04ca525787aae075a5f90429c4c194
unknown <saadsyed950@gmail.com>
2025-01-11 09:48:10 +0530

    Working on Embedding component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="flex justify-start items-center w-full">
+    <div className="flex justify-center items-center w-full min-h-screen">
       <h1 className="text-4xl text-red-500 font-semibold">Mariyam</h1>
     </div>
   );
